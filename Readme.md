2de582aa341d2004408396caeef384ee8dc75e9f
Shubham Singh <shubh4579@gmail.com>
2025-05-28 21:11:32 +0530

    added forecast,dropdown,localhost,comments,readme.md

---
+++ Readme.md
@@ -0,0 +1,51 @@
+# Weather Forecast Application
+
+A responsive web application that provides current weather and 5-day forecast details for any city or the user's current location. Built using **JavaScript**, **HTML**, **Tailwind CSS**, and the **OpenWeatherMap API**.
+
+---
+
+## Screenshots
+
+- Homepage
+  ![Homepage](./images/Screenshot-3.png)
+- Search Result
+  ![Search](./images/Screenshot-2.png)
+- Search Result from other City
+  ![search2](./images/Screenshot-1.png)
+
+## Features
+
+- Search weather by **city name**
+- Get weather of **current location** using Geolocation API
+- View **5-day extended forecast**
+- Display of temperature, humidity, wind speed, and weather icons
+- **Recent search history** dropdown (stored in browser's localStorage)
+- Graceful error handling for invalid inputs or API issues
+- Fully **responsive design** for desktop, tablet, and mobile devices
+
+---
+
+## Technologies Used
+
+- HTML5
+- Tailwind CSS
+- JavaScript (ES6)
+- OpenWeatherMap API
+- LocalStorage API
+- Geolocation API
+
+---
+
+## Getting Started
+
+### Clone the repository
+
+```bash
+git clone https://github.com/your-username/weather-forecast-app.git
+cd weather-forecast-app
+
+npm run dev  //This command watches changes in src/input.css and compiles them to output.css using Tailwind CLI.
+
+//Once Tailwind watches is running then open it with live server.
+
+```
--- src/script.js
+++ src/script.js
@@ -1,25 +1,28 @@
+// API Configuration
 const apiKey = "e6c85ac6cdb1a42e662e3894c1756b91";
 const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-
+//DOM Element Selection
 const searchBox = document.querySelector(".search-box");
 const searchBtn = document.querySelector(".search-btn");
 const locationBtn = document.querySelector(".live-location");
 const forecastBox = document.querySelector(".forecast");
-
+//get current location function that uses api which is taking latitude and longitude of users
 function getCurrentLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       async (position) => {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
+        //fetch api using latitude and longitude
         const res = await fetch(
           `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
         );
         const data = await res.json();
-
+        // Display Weather data
         exactFunc(data);
         getForecast(data.name);
+        updateSearchHistory(data.name);
       },
       (error) => {
         alert("location access denied or unavailable");
@@ -29,10 +32,47 @@ function getCurrentLocation() {
     alert("Geolocation is not supported by this browser.");
   }
 }
+//update search history fiunction  in dropdown
+function updateSearchHistory(city) {
+  let history = JSON.parse(localStorage.getItem("weatherSearchHistory")) || [];
+  //remove duplicate city name and add new city to top
+  history = history.filter((item) => item.toLowerCase() !== city.toLowerCase());
+  history.unshift(city);
+  //limit to 5 search history
+  if (history.length > 5) history.pop();
+  //save to local storage
+  localStorage.setItem("weatherSearchHistory", JSON.stringify(history));
+  //update dropdown
+  SearchDropdown();
+}
+//Display recent search
+function SearchDropdown() {
+  const dropdown = document.querySelector(".search-history");
+  const history =
+    JSON.parse(localStorage.getItem("weatherSearchHistory")) || [];
+  //if no search history is there then dropdown will hidden
+  if (history.length === 0) {
+    dropdown.classList.add("hidden");
+    return;
+  }
+  dropdown.classList.remove("hidden");
+  dropdown.innerHTML = "";
+
+  //Add each city as option
+
+  history.forEach((city) => {
+    const option = document.createElement("option");
+    option.value = city;
+    option.textContent = city;
+    dropdown.appendChild(option);
+  });
+}
+
+// Featch and display weather data
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-
+  //diplay error message when invalid city
   if (response.status === 404) {
     document.querySelector(".error-msg").classList.remove("hidden");
     document.querySelector(".weather").classList.add("hidden");
@@ -42,8 +82,11 @@ async function checkWeather(city) {
     var data = await response.json();
     exactFunc(data);
     getForecast(city);
+    updateSearchHistory(city);
   }
 }
+
+//Create Forecast Card HTML
 const createWeatherCard = (weatherItem) => {
   return `<div class="bg-purple-700 rounded-2xl p-4 flex flex-col items-center text-center">
                 <p class="text-sm  mb-2">${weatherItem.dt_txt.split(" ")[0]}</p>
@@ -69,11 +112,14 @@ const createWeatherCard = (weatherItem) => {
                 <p class="text-xs ">Wind Speed</p>
             </div>`;
 };
+
+//Fetch 5 days weather forecast
 async function getForecast(city) {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`
   );
   const data = await response.json();
+  //Extract one forecast per day and added in empty array
   const uniqueForcastdays = [];
   const fiveDaysForecast = data.list.filter((forecast) => {
     const forecastDate = new Date(forecast.dt_txt).getDate();
@@ -81,13 +127,14 @@ async function getForecast(city) {
       return uniqueForcastdays.push(forecastDate);
     }
   });
+  //Clear old forecast and display new one
   forecastBox.innerHTML = "";
   console.log(fiveDaysForecast);
   fiveDaysForecast.slice(1, 6).forEach((weatherItem) => {
     forecastBox.insertAdjacentHTML("beforeend", createWeatherCard(weatherItem));
   });
 }
-
+//As per weather condition diplay icon
 function getWeatherImage(weatherMain) {
   switch (weatherMain) {
     case "Clear":
@@ -114,7 +161,7 @@ function getWeatherImage(weatherMain) {
       return "../images/default.png";
   }
 }
-
+//update ui with api data
 function exactFunc(data) {
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
@@ -130,15 +177,28 @@ function exactFunc(data) {
   document.querySelector(".forecast-heading").textContent =
     "Next 5-Days Weather Forecast: " + data.name;
 }
-
+//Search button Event Listener
 searchBtn.addEventListener("click", () => {
-  const searchBoxTrimmedValue = searchBox.value.trim();
+  const searchBoxTrimmedValue = searchBox.value.trim().toUpperCase();
+  //empty search box then trigger alert message
   if (searchBoxTrimmedValue === "") {
     alert("Enter the City Name");
     return;
   }
   checkWeather(searchBoxTrimmedValue);
 });
+//Live location button click
 locationBtn.addEventListener("click", () => {
   getCurrentLocation();
 });
+// Dropdown Selection
+document.querySelector(".search-history").addEventListener("change", (e) => {
+  const selectedCity = e.target.value;
+  // A valid city was selected
+  if (selectedCity && selectedCity !== "Recent Searches") {
+    //call checkweather function to display weather using dropdown
+    checkWeather(selectedCity);
+  }
+});
+//call dropdown
+SearchDropdown();
